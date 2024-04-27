90ac7a17561b39800853f91d4294d75f2368c36a
FelinaeBlanc <arthurlenne2@gmail.com>
2024-04-27 15:11:12 +0200

    Fruncate (q6) + Scheme

--- src/ensishell.c
+++ src/ensishell.c
@@ -10,6 +10,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/wait.h>
 
 #include "variante.h"
 #include "readcmd.h"
@@ -17,6 +18,13 @@
 #ifndef VARIANTE
 #error "Variante non défini !!"
 #endif
+
+// Déclaration fonctions
+struct cmdline *initialise_cmd(char *line);
+void execute_cmd(struct cmdline *l);
+void jobs();
+void addBgProcess(int pid, char* command);
+
 
 /* Guile (1.8 and 2.0) is auto-detected by cmake */
 /* To disable Scheme interpreter (Guile support), comment the
@@ -35,11 +43,9 @@ int question6_executer(char *line)
 	 * pipe and i/o redirection are not required.
 	 */
 
-	printf("Not implemented yet: can not execute %s\n", line);
-    
-	/* Remove this line when using parsecmd as it will free it */
-	free(line);
-	
+	struct cmdline* l = initialise_cmd(line);
+	execute_cmd(l);
+
 	return 0;
 }
 
@@ -59,6 +65,120 @@ void terminate(char *line) {
 	  free(line);
 	printf("exit\n");
 	exit(0);
+}
+
+struct cmdline *initialise_cmd(char *line) {
+	/* parsecmd free line and set it up to 0 */
+	struct cmdline* l = parsecmd(&line);
+
+	/* If input stream closed, normal termination */
+	if (!l) {
+		return l;
+	}
+	
+	if (l->err) {
+		/* Syntax error, read another command */
+		printf("error: %s\n", l->err);
+		return l;
+	}
+
+	if (l->in) printf("in: %s\n", l->in);
+	if (l->out) printf("out: %s\n", l->out);
+	if (l->bg) printf("background (&)\n");
+
+	/* Display each command of the pipe */
+	for (int i=0; l->seq[i]!=0; i++) {
+		char **cmd = l->seq[i];
+		printf("seq[%d]: ", i);
+					for (int j=0; cmd[j]!=0; j++) {
+							printf("'%s' ", cmd[j]);
+					}
+		printf("\n");
+	}
+
+	return l;
+}
+
+void execute_cmd(struct cmdline *l) {
+	// Création d'un processus fils pour exécuter les commandes
+	int pid_child = fork(); 
+	
+	if (pid_child == 0) { // Processus fils qui exécute les commandes une par une 
+		int pipefd[2];
+		int prevPipefd[2];
+
+		for (int i=0; l->seq[i]!=0; i++) {
+			char **cmd = l->seq[i]; // Command to execute
+			int isNextPipe = l->seq[i+1]!=0; // Si la commande suivante est un pipe
+			pipe(pipefd);
+			
+
+			int pid_child_command = fork(); 
+			if(pid_child_command == 0) { // Processus fils
+
+				// Redirection de l'entrée et de la sortie
+				int in, out;
+				if (l->in) { // Entrée
+					in = open(l->in, O_RDONLY);
+					dup2(in, STDIN_FILENO);
+					close(in);
+				}
+				if (l->out) { // Sortie
+					out = open(l->out, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					ftruncate(out, 0);
+					dup2(out, STDOUT_FILENO);
+					close(out);
+				}
+
+				// Redirection des pipes
+				if (i != 0) { // If it's not the first command
+					dup2(prevPipefd[0], STDIN_FILENO);
+				}
+				if (isNextPipe) { // If there is a next command (pipe) 
+					dup2(pipefd[1], STDOUT_FILENO);
+				}
+
+				if (i!=0){
+					close(prevPipefd[0]);
+					close(prevPipefd[1]);
+				}
+				close(pipefd[0]); 
+				if (!isNextPipe) {
+					close(pipefd[1]);
+				}
+				if ( strcmp(cmd[0],"jobs") == 0 ){ // Fonctions builtin 
+					jobs();
+					exit(0);
+				}
+				execvp(cmd[0], cmd);
+				exit(1);
+			} else { // Processus père
+				close(prevPipefd[0]);
+				close(prevPipefd[1]);
+
+				prevPipefd[0] = pipefd[0];
+				prevPipefd[1] = pipefd[1];
+
+				if (!isNextPipe) {
+					close(pipefd[0]);
+					close(pipefd[1]);
+				}
+
+				int status;
+				waitpid(pid_child_command, &status, 0); // Attente du processus fils
+			}
+
+		}
+		exit(0);
+	} else { // Processus père
+		if (!l->bg){
+			int status;
+			waitpid(pid_child, &status, 0);
+		} else {
+			addBgProcess(pid_child, l->seq[0][0]);
+		}
+
+	}
 }
 
 // Historique des processus en arrière plan
@@ -113,7 +233,7 @@ void jobs() {
 		bgProcess *current = next;
 		next = current->next;
 		
-        if (waitpid(current->pid, &status, WNOHANG) > 0) {
+        if (waitpid(current->pid, &status, WNOHANG) != 0) {
 
 			printf("Process with PID %d exited with status %d\n", current->pid, WEXITSTATUS(status));
 			removeBgProcess(current->pid);
@@ -135,7 +255,6 @@ int main() {
 	while (1) {
 		struct cmdline *l;
 		char *line=0;
-		int i, j;
 		char *prompt = "ensishell>";
 
 		/* Readline use some internal memory structure that
@@ -162,119 +281,9 @@ int main() {
                 }
 #endif
 
-		/* parsecmd free line and set it up to 0 */
-		l = parsecmd( & line);
 
-		/* If input stream closed, normal termination */
-		if (!l) {
-			terminate(0);
-		}
-		
-
-		
-		if (l->err) {
-			/* Syntax error, read another command */
-			printf("error: %s\n", l->err);
-			continue;
-		}
-
-		if (l->in) printf("in: %s\n", l->in);
-		if (l->out) printf("out: %s\n", l->out);
-		if (l->bg) printf("background (&)\n");
-
-		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
-		}
-
-		// Création d'un processus fils pour exécuter les commandes
-		int pid_child = fork(); 
-		
-		if (pid_child == 0) { // Processus fils qui exécute les commandes une par une 
-			int pipefd[2];
-			int prevPipefd[2];
-
-			for (i=0; l->seq[i]!=0; i++) {
-		    	char **cmd = l->seq[i]; // Command to execute
-				int isNextPipe = l->seq[i+1]!=0; // Si la commande suivante est un pipe
-
-				if (isNextPipe) {
-					pipe(pipefd);
-				}
-
-				int pid_child_command = fork(); 
-				if(pid_child_command == 0) { // Processus fils
-					int in, out;
-					if (l->in) { // Entrée
-						if ((in = open(l->in, O_RDONLY)) < 0) {
-							perror("open");
-							exit(EXIT_FAILURE);
-						}
-						dup2(in, STDIN_FILENO);
-						close(in);
-					}
-					if (l->out) { // Sortie
-						if ((out = open(l->out, O_WRONLY | O_CREAT | O_TRUNC, 0644)) < 0) {
-							perror("open");
-							exit(EXIT_FAILURE);
-						}
-						dup2(out, STDOUT_FILENO);
-						close(out);
-					}
-
-					if (i != 0) { // If it's not the first command
-						dup2(prevPipefd[0], STDIN_FILENO);
-					}
-					if (isNextPipe) { // If there is a next command (pipe) 
-						dup2(pipefd[1], STDOUT_FILENO);
-					}
-
-					close(prevPipefd[0]);
-					close(prevPipefd[1]);
-					close(pipefd[0]); 
-					if (!isNextPipe) {
-						close(pipefd[1]);
-					}
-
-					if ( strcmp(cmd[0],"jobs") == 0 ){ // Fonctions builtin 
-						jobs();
-						exit(0);
-					}
-					execvp(cmd[0], cmd);
-					exit(1);
-				} else { // Processus père
-					close(prevPipefd[0]);
-					close(prevPipefd[1]);
-
-					prevPipefd[0] = pipefd[0];
-					prevPipefd[1] = pipefd[1];
-
-					if (!isNextPipe) {
-						close(pipefd[0]);
-						close(pipefd[1]);
-					}
-
-					printf("bruh \n");
-					int status;
-					waitpid(pid_child_command, &status, 0); // Attente du processus fils
-				}
-
-			}
-			exit(0);
-		} else { // Processus père
-			if (!l->bg){
-				int status;
-				waitpid(pid_child, &status, 0);
-			} else {
-				addBgProcess(pid_child, l->seq[0][0]);
-			}
-
-		}
+		l = initialise_cmd(line);
+		execute_cmd(l);
 	}
 
 }
